a49c34f42cea2ec59517339bb3511c788d2fdb13
plzslp <gyutann1171@gmail.com>
2026-03-13 16:01:52 +0900

    feat: 거래대금, 상장 시가 총액 타입 변경
    --- 본문 ---
    - tradingPrice, marketTotalAmount의 타입을 Long에서 BigInteger로 변경
    
    --- 꼬리말 ---
    fix: #7

---
+++ src/main/java/com/sprint/project/findex/indexinfo/entity/IndexInfo.java
@@ -0,0 +1,11 @@
+package com.sprint.project.findex.indexinfo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class IndexInfo {
+  @Id
+  Long id;
+  //추후 연동
+}
