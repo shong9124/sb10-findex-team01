f8492835d7f27ebbfe93baeddfa6a9d0bd55d8bc
Nooroong <sjiyeon759@gmail.com>
2026-03-17 11:01:45 +0900

    feat: IndexData, IndexInfo Repository 작성
    --- 본문 (선택 사항, 상세 설명이 필요한 경우) ---
    - IndexData: 지수 정보와 기준날짜를 바탕으로 지수 데이터를 조회하는 메서드를 작성했습니다.
    - IndexInfo: id 리스트를 바탕으로 지수 정보를 조회하는 메서드를 작성했습니다.
    
    --- 꼬리말 (선택 사항, 이슈 번호 연결) ---
    feat: #12

--- src/main/java/com/sprint/project/findex/repository/IndexInfoRepository.java
+++ src/main/java/com/sprint/project/findex/repository/IndexInfoRepository.java
@@ -1,8 +1,10 @@
 package com.sprint.project.findex.repository;
 
 import com.sprint.project.findex.entity.IndexInfo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long> {
 
+  List<IndexInfo> findByIdIn(List<Long> ids);
 }
