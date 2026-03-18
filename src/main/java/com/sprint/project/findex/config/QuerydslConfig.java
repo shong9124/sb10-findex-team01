dce83304c46e53fef3ae81774f700b8aaea0a071
Nooroong <sjiyeon759@gmail.com>
2026-03-18 11:06:58 +0900

    feat: 임시 커밋
    --- 본문 ---
    지수 정보 요청을 보내고 응답을 DTO 에 매핑하였다.
    
    --- 꼬리말 ---
    fix: #1

--- src/main/java/com/sprint/project/findex/config/QuerydslConfig.java
+++ src/main/java/com/sprint/project/findex/config/QuerydslConfig.java
@@ -3,14 +3,16 @@ package com.sprint.project.findex.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RequiredArgsConstructor
 @Configuration
-public class QuerydslConfig {
+public class QueryDslConfig {
 
   @PersistenceContext
-  private EntityManager entityManager;
+  private final EntityManager entityManager;
 
   @Bean
   public JPAQueryFactory jpaQueryFactory() {
