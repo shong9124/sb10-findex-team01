09dd4a448cb1c1b69f395fb7296e67aa7ca2f244
zinuzanu <wsx104301@naver.com>
2026-03-18 11:30:15 +0900

    chore: PR merge 완료 후 코드 재배치

--- src/main/java/com/sprint/project/findex/repository/IndexInfoRepository.java
+++ src/main/java/com/sprint/project/findex/repository/IndexInfoRepository.java
@@ -2,12 +2,18 @@ package com.sprint.project.findex.repository;
 
 import com.sprint.project.findex.entity.IndexInfo;
 import com.sprint.project.findex.repository.querydsl.IndexInfoQDSLRepository;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long>,
     IndexInfoQDSLRepository {
 
   Optional<IndexInfo> findByIndexClassificationAndIndexName(String indexClassification,
       String indexName);
+
+  @Query("SELECT i FROM IndexInfo i WHERE CONCAT(i.indexClassification, '_', i.indexName) IN :keys AND i.isDeleted = 'ACTIVE'")
+  List<IndexInfo> findByKeyIn(@Param("keys") List<String> keys);
 }
